477e618959930a2e79a402356821f0adc5294a7d
gabriel-dp <gabriel.meira.2004@gmail.com>
2023-09-24 11:00:05 -0300

    :sparkles: Implement responsiveness to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,28 +1,45 @@
-import { Link, NavLink } from "react-router-dom";
-import { NavContainer, NavHome } from "./styles";
+import { useState } from "react";
 
-function NavComponent(props: { title: string; path: string }) {
-	return (
-		<li>
-			<NavLink to={props.path}>{props.title}</NavLink>
-		</li>
-	);
-}
+import { Link, NavLink } from "react-router-dom";
+import { MdMenu, MdOutlineClose } from "react-icons/md";
+import { NavContainer, NavHome, MenuIconContainer } from "./styles";
 
 export default function Navbar() {
+	const [menuActive, setMenuActive] = useState(false);
+
+	function handleMenuClick() {
+		console.log(!menuActive);
+		setMenuActive(!menuActive);
+	}
+
+	function closeMenu() {
+		setMenuActive(false);
+	}
+
+	function NavComponent(props: { title: string; path: string }) {
+		return (
+			<li>
+				<NavLink onClick={closeMenu} to={props.path}>
+					{props.title}
+				</NavLink>
+			</li>
+		);
+	}
+
 	return (
 		<NavContainer>
-			<NavHome>
+			<NavHome onClick={closeMenu}>
 				<Link to="/">
 					<h1>LOGO</h1>
 				</Link>
 			</NavHome>
-			<ul>
+			<ul className={menuActive ? "menu-on" : "menu-off"}>
 				<NavComponent title="Tabelas" path="/tabelas" />
 				<NavComponent title="Jogos" path="/jogos" />
 				<NavComponent title="Times" path="/times" />
 				<NavComponent title="Jogadores" path="/jogadores" />
 			</ul>
+			<MenuIconContainer onClick={handleMenuClick}>{menuActive ? <MdOutlineClose /> : <MdMenu />}</MenuIconContainer>
 		</NavContainer>
 	);
 }
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
-const ACTIVE_BORDER_SIZE = 5;
-
 export const NavContainer = styled.nav`
 	width: 100%;
-	padding: 0 1.5rem;
+	height: 3rem;
+	padding: 0 2rem;
 	background-color: ${(props) => props.theme.primary};
 	color: ${(props) => props.theme.primaryText};
 
@@ -13,35 +12,95 @@ export const NavContainer = styled.nav`
 	align-items: center;
 
 	ul {
+		height: 100%;
 		list-style: none;
 		display: flex;
 		gap: 1rem;
+		transition: right ease-in-out 0.5s;
 	}
 
 	li {
 		height: 100%;
 		display: flex;
 		justify-content: center;
-
-		:hover {
-			text-shadow: 0px 0px 2px ${(props) => props.theme.primaryText}; // BOLD
-		}
-
-		.active {
-			text-shadow: 0px 0px 2px ${(props) => props.theme.primaryText}; // BOLD
-			border-bottom: ${ACTIVE_BORDER_SIZE}px solid ${(props) => props.theme.primaryText};
-		}
+		align-items: center;
 
 		a {
-			border-top: ${ACTIVE_BORDER_SIZE}px solid transparent;
-			border-bottom: ${ACTIVE_BORDER_SIZE}px solid transparent;
 			height: 100%;
-			padding: 0.75rem;
+			color: inherit;
+			text-decoration: none;
+			padding: 0 0.75rem;
+			transition: all ease-in-out 0.25s;
+
 			display: flex;
 			align-items: center;
-			transition: all ease-in-out 0.2s;
+			position: relative;
+
+			&::before {
+				content: "";
+				position: absolute;
+				width: 1.5rem;
+				height: 2px;
+				border-radius: 1rem;
+				background-color: transparent;
+				bottom: 0.5rem;
+				left: 50%;
+				transform: translateX(-50%);
+				transition: background ease-in-out 0.25s;
+			}
+
+			&:hover,
+			&.active {
+				text-shadow: 0px 0px 2px ${(props) => props.theme.primaryText}; // BOLD
+				&::before {
+					background-color: ${(props) => props.theme.primaryText};
+				}
+			}
 		}
 	}
+
+	@media (max-width: 768px) {
+		ul {
+			flex-direction: column;
+			align-items: flex-end;
+			justify-content: flex-start;
+			background-color: ${(props) => props.theme.primary};
+
+			position: fixed;
+			right: 0;
+			top: 3rem;
+			height: 100vh;
+			padding: 1rem 2rem 1rem 3rem;
+			padding-left: 3rem;
+
+			a {
+				padding: 0;
+
+				&::before {
+					width: 0.35rem;
+					height: 0.35rem;
+					left: -1rem;
+					bottom: 50%;
+					transform: translateY(50%);
+				}
+			}
+		}
+
+		.menu-off {
+			right: -50%;
+		}
+
+		li {
+			height: auto;
+		}
+	}
+`;
+
+export const NavHome = styled.div`
+	height: 100%;
+	padding: 0 0.25rem;
+	display: flex;
+	align-items: center;
 
 	a {
 		color: inherit;
@@ -49,10 +108,13 @@ export const NavContainer = styled.nav`
 	}
 `;
 
-export const NavHome = styled.div`
-	height: 100%;
-	display: flex;
-	align-items: center;
-	margin: 0 2.5rem;
+export const MenuIconContainer = styled.div`
+	font-size: 1.25rem;
+	display: none;
+	cursor: pointer;
+
+	@media (max-width: 768px) {
+		display: block;
+	}
 `;
 
