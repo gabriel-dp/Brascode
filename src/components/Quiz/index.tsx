c3cfc6f099a01fc4b8c51c9ccbf5d438cbad27af
gabriel-dp <gabriel.meira.2004@gmail.com>
2023-12-04 23:16:12 -0300

    :bug: Fix null player image in quiz

--- src/components/Quiz/index.tsx
+++ src/components/Quiz/index.tsx
@@ -21,7 +21,7 @@ export default function Quiz<T extends TeamI | PlayerI>(props: QuizI<T>) {
 	const options = useMemo(() => {
 		setSelected(null);
 		return shuffleElements(
-			props.elements.filter((element) => element.imagem != undefined || element.imagem == "")
+			props.elements.filter((element) => element.imagem != undefined && element.imagem != "")
 		).slice(0, props.quantity);
 	}, [props]);
 	const correct = useMemo(() => shuffleElements(options)[0], [options]);
